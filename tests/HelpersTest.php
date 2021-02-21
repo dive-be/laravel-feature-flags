bd5b40d8e397fa5f7ee558b55888984ee8aa4000
Muhammed Sarı <muhammed@dive.be>
2021-02-21 18:24:19 +0100

    Helpers tests

---
+++ tests/HelpersTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests;
+
+use Tests\Factories\FeatureFactory;
+
+test('feature finds and returns a specific feature', function () {
+    $featureA = FeatureFactory::new()->create();
+
+    $featureB = feature($featureA->name, $featureA->scope);
+
+    expect($featureA->is($featureB))->toBeTrue();
+});
+
+test('feature_disabled determines whether a feature has been disabled', function () {
+    $featureA = FeatureFactory::new()->create();
+    $featureB = FeatureFactory::new()->isDisabled()->create();
+
+    expect(feature_disabled($featureA->name, $featureA->scope))->toBeFalse();
+    expect(feature_disabled($featureB->name, $featureB->scope))->toBeTrue();
+});
+
+test('feature_enabled determines whether a feature has been enabled', function () {
+    $featureA = FeatureFactory::new()->create();
+    $featureB = FeatureFactory::new()->isDisabled()->create();
+
+    expect(feature_enabled($featureA->name, $featureA->scope))->toBeTrue();
+    expect(feature_enabled($featureB->name, $featureB->scope))->toBeFalse();
+});
