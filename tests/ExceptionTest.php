100b6c08887a0ee6e75ebf47e7b6811069b7888a
Muhammed Sarı <muhammed@dive.be>
2021-02-21 18:53:53 +0100

    Exception test

---
+++ tests/ExceptionTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests;
+
+use Dive\FeatureFlags\Exceptions\UnknownFeatureException;
+
+test('unknown feature is makeable', function () {
+    $exception = UnknownFeatureException::make('dive', 'interactive');
+
+    expect($exception->getMessage())->toBe('The requested feature interactive:dive could not be found.');
+});
