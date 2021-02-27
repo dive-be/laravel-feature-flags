3a2f4630ed7c89c1bffa1ac46fa12612d1df5b4c
Muhammed Sarı <muhammed@dive.be>
2021-02-28 00:04:32 +0100

    Clean up provider

--- src/FeatureFlagsServiceProvider.php
+++ src/FeatureFlagsServiceProvider.php
@@ -18,20 +18,14 @@ class FeatureFlagsServiceProvider extends ServiceProvider
     public function boot()
     {
         if ($this->app->runningInConsole()) {
-            $this->registerPublishables();
-
-            $this->commands([
-                InstallPackageCommand::class,
-                ListFeatureCommand::class,
-                ToggleFeatureCommand::class,
-            ]);
+            $this->registerCommands();
+            $this->registerConfig();
+            $this->registerMigration();
         }
 
-        $this->registerBladeDirectives();
-
-        $this->app->make('router')->aliasMiddleware('feature', EnsureFeatureIsEnabled::class);
-
         $this->registerAtGate();
+        $this->registerDirectives();
+        $this->registerMiddleware();
     }
 
     public function register()
@@ -56,29 +50,51 @@ class FeatureFlagsServiceProvider extends ServiceProvider
         });
     }
 
-    private function registerBladeDirectives()
+    private function registerCommands()
     {
-        $this->app['blade.compiler']->directive('disabled', fn ($expression) => "<?php if (feature_disabled({$expression})) : "
+        $this->commands([
+            InstallPackageCommand::class,
+            ListFeatureCommand::class,
+            ToggleFeatureCommand::class,
+        ]);
+    }
+
+    private function registerConfig()
+    {
+        $config = 'feature-flags.php';
+
+        $this->publishes([
+            __DIR__.'/../config/'.$config => $this->app->configPath($config),
+        ], 'config');
+    }
+
+    private function registerDirectives()
+    {
+        $this->app->make('blade.compiler')->directive('disabled', fn ($expression) => "<?php if (feature_disabled({$expression})) : "
             .PHP_EOL.'if (isset($message)) { $__messageOriginal = $message; } '
             .PHP_EOL.'$message = feature('.$expression.')->message; ?>');
 
-        $this->app['blade.compiler']->directive('enabled', fn () => '<?php else: ?>');
+        $this->app->make('blade.compiler')->directive('enabled', fn () => '<?php else: ?>');
 
-        $this->app['blade.compiler']->directive('enddisabled', fn () => '<?php unset($message);'
+        $this->app->make('blade.compiler')->directive('enddisabled', fn () => '<?php unset($message);'
             .PHP_EOL.'if (isset($__messageOriginal)) { $message = $__messageOriginal; } '
             .PHP_EOL.'endif ?>');
     }
 
-    private function registerPublishables()
+    private function registerMiddleware()
+    {
+        $this->app->make('router')->aliasMiddleware('feature', EnsureFeatureIsEnabled::class);
+    }
+
+    private function registerMigration()
     {
         if (! class_exists('CreateFeaturesTable')) {
+            $timestamp = date('Y_m_d_His', time());
+            $stub = __DIR__.'/../database/migrations/create_features_table.php.stub';
+
             $this->publishes([
-                __DIR__.'/../database/migrations/create_features_table.php.stub' => $this->app->databasePath('migrations/'.date('Y_m_d_His', time()).'_create_features_table.php'),
+                $stub => $this->app->databasePath("migrations/'{$timestamp}'_create_features_table.php"),
             ], 'migrations');
         }
-
-        $this->publishes([
-            __DIR__.'/../config/feature-flags.php' => $this->app->configPath('feature-flags.php'),
-        ], 'config');
     }
 }
