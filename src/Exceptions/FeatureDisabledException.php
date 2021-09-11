770efc613accf28cbce9f727a85798cb4d87018a
Muhammed Sari <muhammed@dive.be>
2021-09-11 15:03:31 +0200

    Pass feature instance with the exception

--- src/Exceptions/FeatureDisabledException.php
+++ src/Exceptions/FeatureDisabledException.php
@@ -7,8 +7,17 @@ use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class FeatureDisabledException extends AccessDeniedHttpException
 {
+    public Feature $feature;
+
     public static function make(Feature $feature): self
     {
-        return new self($feature->getMessage());
+        return (new self($feature->getMessage()))->withFeature($feature);
+    }
+
+    public function withFeature(Feature $feature): self
+    {
+        $this->feature = $feature;
+
+        return $this;
     }
 }
